4ea499d3d8faac6c0b525c4601697783442ce5f4
Anna Moskalchuk <anna@crystax.net>
2020-12-18 11:15:17 +0200

    fix: about columns db

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
-    comleted = db.Column(db.Boolean, nullable=True, default=True)
+    completed = db.Column(db.Boolean, nullable=True, default=True)
 
     def __repr__(self):
         return '<Todo {self.id} {self.description} {self.completed}>'
